2856ef3d1411beac130c33fa62ac27384279cc0b
sh91c <52781333+sh91c@users.noreply.github.com>
2020-11-07 09:26:55 +0900

    cookie

---
+++ nodejs_study/lec/index4/cookie/cookie.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+  console.log(req.url, req.headers.cookie);
+  res.writeHead(200, { 'Set-Cookie' : 'mycookie=test'});
+  res.end('Hello Cookie');
+})
+  .listen(8082, () => console.log('8082번 포트에서 서버 대기 중입니다!'));
