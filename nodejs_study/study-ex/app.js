a7fb5c69d741239dbbeefe713933926ee79454c0
sh91c <52781333+sh91c@users.noreply.github.com>
2020-11-08 20:00:17 +0900

    study

--- nodejs_study/study-ex/app.js
+++ nodejs_study/study-ex/app.js
@@ -1,46 +1,47 @@
 const express = require('express');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const dotenv = require('dotenv');
 const path = require('path');
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
 
-dotenv.config();
+const { sequelize } = require('./models'); // sequelize : ORM
 
 const indexRouter = require('./routes/index');
-const userRouter = require('./routes/user');
-const routerGrpRouter = require('./routes/routerGrp');
 
 const app = express();
 app.set('port', process.env.PORT || 9090);
+app.set('view engine', 'html'); // nunjucks engine
 
-app.use(morgan('dev'));
-app.use('/', express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-app.use(express.urlencoded({ extended : false }));
-app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-  resave : false,
-  saveUninitialized : false,
-  secret : process.env.COOKIE_SECRET,
-  cookie : {
-    httpOnly: true,
-    secure : false,
-  },
-  name : 'session-cookie',
-}));
 
-app.use('/', indexRouter);
-app.use('/user', userRouter); // /user + / => /user/
-app.use('/abc', routerGrpRouter);
-
-app.use((req, res, next) => {
-  res.status(404).send('Not Found');
-}); // 굳이 경로를 *로 작성하지 않아도 됌
-
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).send(err.message);
+nunjucks.configure('views', {
+  express : app,
+  watch : true,
 });
 
-app.listen(app.get('port'), () => console.log(app.get('port'), `번 포트 서버 대기 중`));
+sequelize.sync({ force : false })
+  .then(() => console.log('DB connected.'))
+  .catch((err) => console.error(err.message)); // sequelize.sync를 통해서 mysql을 연결할 수 있다 ***
+
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended : false }));
+
+
+app.use('/', indexRouter);
+
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(app.get('port'), () => console.log(app.get('port'), '번 포트에서 대기 중'));
